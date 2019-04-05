6a0e8f295145ad44fda44ace05df190922458281
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2019-04-06 01:20:50 +0200

    implementanto el middleware de log en v2

--- servr/v2/main.go
+++ servr/v2/main.go
@@ -19,8 +19,8 @@ func main() {
 
 	r := http.NewServeMux()
 
-	r.HandleFunc("/api/v1/hello", HelloChameleon)
-	r.HandleFunc("/api/v1/sum", Sum)
+	r.HandleFunc("/api/v1/hello", LogIncomingRequest(HelloChameleon))
+	r.HandleFunc("/api/v1/sum", LogIncomingRequest(Sum))
 
 	s := http.Server{
 		Addr:         addr,
@@ -64,6 +64,14 @@ func Sum(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(w, fmt.Sprintf("Opps!, algo salió mal. Error: %v", err), http.StatusInternalServerError)
 		return
+	}
+}
+
+// LogIncomingRequest logs incoming request
+func LogIncomingRequest(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%v | [%v] %v - %v \n", r.Proto, r.RemoteAddr, r.Method, r.RequestURI)
+		f(w, r)
 	}
 }
 
