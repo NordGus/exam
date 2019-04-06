bf7c5e577d1f7c597cb9145ac52b9b090bdc00ff
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2019-04-06 23:03:52 +0200

    refactorizando el proceso a un metodo

--- logrest/main.go
+++ logrest/main.go
@@ -27,19 +27,27 @@ func (m Matriz) Subtraction() float64 {
 	return t
 }
 
-func main() {
-	logger := log.New(os.Stdout, "logrest ", log.LstdFlags|log.Lshortfile)
-	lf, err := os.OpenFile("logrestfile.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+// SubstractAndLog es un metodo que calcula la resta de todos los valores de una Matriz
+// y guarda el resultado en un fichero de log en disco (filename) con el formato:
+// <nombre_operación>,<fecha>,<hora>,<resultado>. Y devuelve un error en cado de que algo halla fallado
+func (m Matriz) SubstractAndLog(filename string) error {
+	r := m.Subtraction()
+	lf, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
-		logger.Println(err)
-		os.Exit(7)
+		return err
 	}
 	defer lf.Close()
+	_, err = lf.Write([]byte(fmt.Sprintf("%s,%s,%v\n", "resta", time.Now().Format("2006-01-02,15:04"), r)))
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
+func main() {
+	logger := log.New(os.Stdout, "logrest ", log.LstdFlags|log.Lshortfile)
 	m := generateRandomMatrix()
-	r := m.Subtraction()
-
-	err = writeToLogFile(r, lf)
+	err := m.SubstractAndLog("logrestfile.log")
 	if err != nil {
 		logger.Println(err)
 		os.Exit(7)
@@ -62,9 +70,4 @@ func generateRandomMatrix() Matriz {
 		}
 	}
 	return m
-}
-
-func writeToLogFile(result float64, logfile *os.File) error {
-	_, err := logfile.Write([]byte(fmt.Sprintf("%s,%s,%v\n", "resta", time.Now().Format("2006-01-02,15:04"), result)))
-	return err
 }
